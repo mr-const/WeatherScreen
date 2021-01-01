7aae76ff2890e71232104d6824e5f9969c336ffe
Konst Kolesnichenko <kolesnichenko@gmail.com>
2021-01-01 22:02:53 +0200

    * base dependencies for eink are built

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>     // https://github.com/bblanchon/ArduinoJson
+#include <WiFi.h>            // Built-in
+#include "time.h"
+#include <SPI.h>
+#define  ENABLE_GxEPD2_display 0
+#include <GxEPD2_BW.h>
+#include <GxEPD2_3C.h>
 
 void setup() {
   // put your setup code here, to run once:
